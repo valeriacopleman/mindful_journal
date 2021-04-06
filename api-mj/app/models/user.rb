7daed2a14a8ab6ecf4dbbfa46bdab00f854a9631
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-06 00:21:53 -0700

    fixed model

--- api-mj/app/models/user.rb
+++ api-mj/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :prompts, :manifestations 
+    has_many :prompts
+    has_many :manifestations 
     validates :name, presence: true
     validates :name, uniqueness: true
 
